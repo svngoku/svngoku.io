9b4134269f5375dbfa74423334f5c055b132ad12
Svngoku <marymarty104@gmail.com>
2019-01-06 03:16:17 +0100

    👨🏾‍💻

---
+++ js/channel.js
@@ -0,0 +1,20 @@
+console.log('🤯')
+// translation test
+let lang = [ 'fr' , 'en']
+let traduction =  {
+  fr : {
+    py : "Python un langage de programmation interprété, de haut niveau, à usage général. Créé par Guido van Rossum et publié pour la première fois en 1991, Python repose sur une philosophie de conception qui met l’accent sur la lisibilité du code, en utilisant notamment des espaces significatifs. Il fournit des constructions qui permettent une programmation claire à la fois à petite et à grande échelle. En juillet 2018, Van Rossum a démissionné en tant que leader de la communauté linguistique."
+  },
+  en : {
+    py : "Python an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python has a design philosophy that emphasizes code readability, notably using significant whitespace. It provides constructs that enable clear programming on both small and large scales.[26] In July 2018, Van Rossum stepped down as the leader in the language community."
+  }
+}
+// prompt to choose your languages
+// let langue = prompt('Entrer la langue souhaité ')
+//   if (langue == 'en') {
+//     console.log(traduction.en.py);
+//   } else if( langue == 'fr') {
+//       console.log(traduction.fr.py);
+//   } else {
+//     console.log('Langue indisponible || language not found')
+//   }
---
+++ js/mail.js
@@ -0,0 +1,8 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service : 'gmail',
+  auth: {
+
+  }
+})
