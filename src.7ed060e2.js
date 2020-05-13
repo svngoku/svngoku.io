990089058f2832a27c2652bcf2cfa9f0d31b8714
svngoku <marymarty104@gmail.com>
2020-05-13 04:59:18 +0200

    Updates

--- src.7ed060e2.js
+++ src.7ed060e2.js
@@ -34732,7 +34732,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54538" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54792" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
